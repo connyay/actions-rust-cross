87c63e69660beeee06433093a201c8bf58a09dbe
Connor Hindley <conn.hindley@gmail.com>
2024-01-21 00:12:28 -0700

    Force always cross compiling.
    Allows for consistent pre-build usage.

--- set-cross-compile.sh
+++ set-cross-compile.sh
@@ -10,13 +10,4 @@ if uname -a | grep --quiet --extended-regexp -i "darwin|msys|windows"; then
     exit 0
 fi
 
-# On Linux, we should be able to cross-compile to i586 and i686, but in
-# practice this fails with some crates, notably openssl with the "vendored"
-# feature. This feature makes it compile openssl itself, which fails without
-# cross.
-if echo "$TARGET" | grep --quiet --extended-regexp -i 'x86_64.+linux-(gnu|musl)'; then
-    echo "needs-cross=false" >> $GITHUB_OUTPUT
-    exit 0
-fi
-
 echo "needs-cross=true" >> $GITHUB_OUTPUT
